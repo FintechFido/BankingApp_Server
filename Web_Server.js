23f94471cdbefead5fd602578277d1d88aa9f75c
Suhyeon6801 <k0s3y0@naver.com>
2020-08-12 15:55:52 +0900

    로그인 하면 세션키 발급 후 암호화 해서 DB에 저장

--- Web_Server.js
+++ Web_Server.js
@@ -1,7 +1,20 @@
+const path = require("path");
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var express = require("express"); // npm install express
+var mysql = require("mysql");
+var crypto = require('crypto');
+
+//mysql에 접근 가능한 사용자 확인 여부
+var connection = mysql.createConnection({//TEST DB에 연결.
+    host: "database.c0kvvrvcbjef.ap-northeast-2.rds.amazonaws.com",
+    user: "admin",
+    password: "12344321",
+    database: "bankapp",
+    port: "3306"
+});
+connection.connect();
 
 var key = fs.readFileSync('pr.pem', 'utf-8');
 var certificate =  fs.readFileSync('main_server.crt', 'utf-8');
@@ -9,7 +22,61 @@ var credentials = {key: key, cert: certificate};
 
 var app = express();
 
+app.use(express.urlencoded({ extended: false }));//form에서 데이터를 받아오자!
+
 app.use(express.json());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, "public"))); //to use static asset
+
+app.get("/login", (req,res)=>{
+    console.log("login page");
+    res.render("login");
+});
+
+app.get("/welcome", (req, res)=>{
+   res.render("welcome");
+});
+
+app.post("/login", (req,res)=>{
+    var userId=req.body.userId;
+    var userPassword=req.body.userPassword;
+
+    var sql = "SELECT * FROM bank WHERE ID = ?";
+    connection.query(
+        sql, [userId], function (error, results) {
+        if (error) throw error;
+        else {
+            if (results.length == 0) {
+            res.json("등록되지 않았소.");
+            } else {
+                var dbId=results[0].id;//입력받은 아이디는 암호화로 바꿔서 확인해줘야함.
+                var dbPassword=results[0].password;//입력받은 비밀번호는 해시로 바꿔서 확인해주기!
+
+                if(dbId==userId && dbPassword==userPassword){
+                    console.log("로그인 확인!");
+
+                    //세션키 생성하고 암호화진행
+                    var randomNum=Math.floor(Math.random() * 1000000000) + 1;//랜덤으로 숫자 만들기
+                    var sessionKey=(crypto.createHash('sha512').update(String(randomNum)).digest('base64'));
+
+                    console.log("세션키 암호화해서 만들었다! ==> ", sessionKey);
+
+                    var sql2 = "UPDATE bank SET sessionKey = ? WHERE ID=?";
+                    connection.query(sql2, [sessionKey, userId], function(err, results){
+                        if(err) throw err;
+                        else{
+                            console.log("sessionKey 등록");
+                            res.json(sessionKey);
+                        }
+                    });
+                }
+            }
+        }
+    });
+});
 
 app.get("/get_test", function(req,res){
     console.log("get");
@@ -33,13 +100,12 @@ httpsServer.on('request',function(req,res){
     res.end('hello world\n');
 })
 */
+httpsServer.listen(3000);
 
-// httpsServer.listen(443);
-httpsServer.listen(8080);
 
 /*
-var hostname = '172.31.4.25'; //자신의 private IP 주소
-var port = 8080;
+var hostname = '192.168.0.6'; //자신의 private IP 주소
+var port = 443;
 
 var options = 
 {
