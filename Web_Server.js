30234a57b6da7b7c1b3b68a472bb5ecabce54c02
Suhyeon6801 <k0s3y0@naver.com>
2020-08-14 14:26:24 +0900

    로그인시 id, pw 암호화해서 db값과 비교 후 세션키 발급 후 저장

--- Web_Server.js
+++ Web_Server.js
@@ -7,17 +7,26 @@ var mysql = require("mysql");
 var crypto = require('crypto');
 
 //mysql에 접근 가능한 사용자 확인 여부
-var connection = mysql.createConnection({//TEST DB에 연결.
+var connection = mysql.createConnection({//TEST DB에 연결.(aws)
     host: "database.c0kvvrvcbjef.ap-northeast-2.rds.amazonaws.com",
     user: "admin",
     password: "12344321",
     database: "bankapp",
     port: "3306"
 });
+
+// var connection = mysql.createConnection({//local DB에 연결.
+//     host: "localhost",
+//     user: "root",
+//     password: "8603",
+//     database: "bankapp",
+//     port: "3306"
+// });
+
 connection.connect();
 
-var key = fs.readFileSync('pr.pem', 'utf-8');
-var certificate =  fs.readFileSync('main_server.crt', 'utf-8');
+var key = fs.readFileSync('./keys/sample/pr.pem', 'utf-8');
+var certificate =  fs.readFileSync('./keys/sample/main_server.crt', 'utf-8');
 var credentials = {key: key, cert: certificate};
 
 var app = express();
@@ -44,28 +53,35 @@ app.post("/login", (req,res)=>{
     var userId=req.body.userId;
     var userPassword=req.body.userPassword;
 
+    console.log(userId, userPassword);
+
+    //입력받은 아이디랑 비번은 암호화해서 확인해줘야함.
+    //일방향 암호화 함수 적용해서 db에서 확인해주기!
+    var hash_userId = (crypto.createHash('sha512').update(String(userId)).digest('base64'));
+    var hash_userPassword = (crypto.createHash('sha512').update(String(userPassword)).digest('base64'));
+
     var sql = "SELECT * FROM bank WHERE ID = ?";
     connection.query(
-        sql, [userId], function (error, results) {
+        sql, [hash_userId], function (error, results) {
         if (error) throw error;
         else {
             if (results.length == 0) {
-            res.json("등록되지 않았소.");
+                res.json("등록되지 않았소.");
             } else {
-                var dbId=results[0].id;//입력받은 아이디는 암호화로 바꿔서 확인해줘야함.
-                var dbPassword=results[0].password;//입력받은 비밀번호는 해시로 바꿔서 확인해주기!
+                var dbID=results[0].ID;
+                var dbPW=results[0].PW;
 
-                if(dbId==userId && dbPassword==userPassword){
+                if(dbID==hash_userId && dbPW==hash_userPassword){
                     console.log("로그인 확인!");
 
                     //세션키 생성하고 암호화진행
                     var randomNum=Math.floor(Math.random() * 1000000000) + 1;//랜덤으로 숫자 만들기
-                    var sessionKey=(crypto.createHash('sha512').update(String(randomNum)).digest('base64'));
+                    var sessionKey=(crypto.createHash('sha512').update(String(randomNum)).digest('base64'));//일방향 암호화 함수 적용.
 
                     console.log("세션키 암호화해서 만들었다! ==> ", sessionKey);
 
                     var sql2 = "UPDATE bank SET sessionKey = ? WHERE ID=?";
-                    connection.query(sql2, [sessionKey, userId], function(err, results){
+                    connection.query(sql2, [sessionKey, dbID], function(err, results){
                         if(err) throw err;
                         else{
                             console.log("sessionKey 등록");
@@ -93,43 +109,6 @@ app.post("/post_test", function(req,res){
 })
 
 var httpsServer = https.createServer(credentials, app);
-/*
-httpsServer.on('request',function(req,res){
-    console.log(req.body+" "+res.body);
-    res.writeHead(200);
-    res.end('hello world\n');
-})
-*/
 httpsServer.listen(3000);
-
-
-/*
-var hostname = '192.168.0.6'; //자신의 private IP 주소
-var port = 443;
-
-var options = 
-{
-    key: fs.readFileSync('pr.pem', 'utf-8'),
-    cert: fs.readFileSync('main_server.crt', 'utf-8')
-    //key: fs.readFileSync('pr.pem', 'utf-8'),
-    //cert: fs.readFileSync('main_server.crt', 'utf-8')
-};
-
-https.createServer(options, function(req, res)
-{
-    res.writeHead(200);
-    res.end('hello world\n');
-
-}).listen(port, hostname);
-
-
-/*
-http.createServer(function(req, res){
-    res.writeHead(200);
-    res.end('hello world\n');
-
-}).listen(port, hostname);
-*/
-
 
 console.log('Server running');
