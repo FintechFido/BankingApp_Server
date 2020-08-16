25673d389b65dca97eed402988e35c7add2ea168
ARWA <59240107+angelarwa@users.noreply.github.com>
2020-08-16 19:59:57 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var express = require("express"); // npm install express
+const request = require("request");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //crt의 self-signed 문제 해결
+
+var key = fs.readFileSync('./keys/hidopr.pem', 'utf-8');
+var certificate =  fs.readFileSync('./keys/hido_server.crt', 'utf-8');
+var credentials = {key: key, cert: certificate};
+
+var app = express();
+
+app.use(express.json());
+
+app.get("/get_test", function(req,res){
+    console.log("get");
+    res.writeHead(200);
+    res.end("GET Success");
+})
+
+app.post("/post_test", function(req,res){
+    console.log(req.body);
+    res.send("Post Success");
+})
+
+request.defaults({ //rejectUnauthorized를 false값으로 두어야 https 서버통신 가능
+    strictSSL: false, // allow us to use our self-signed cert for testing
+    rejectUnauthorized: false
+});
+
+request('https://127.0.0.1:3000/get_test', //localhost
+    function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+
+    
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(3002);
+console.log('Server running');
